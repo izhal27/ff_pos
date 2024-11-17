afd8c1ece5c0285292cf32c5cf542d1873cbe5a5
Risal Walangadi <risal.walangadi@gmail.com>
2024-11-18 06:42:21 +0800

    add color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:ff_pos/screens/home.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 0, 48, 73),
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,9 +16,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
       ),
       home: const MyHomePage(title: 'Fast Food POS'),
     );
