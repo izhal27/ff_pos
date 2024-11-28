0767731f5c21b0bfb51c81af656dbab5159d0a7c
Risal Walangadi <risal.walangadi@gmail.com>
2024-11-28 15:54:04 +0800

    add category and food entities

---
+++ lib/domain/entities/category.dart
@@ -0,0 +1,13 @@
+class Category {
+  final int? id;
+  final String name;
+  final String image;
+  final String desc;
+
+  Category({
+    this.id,
+    required this.name,
+    required this.image,
+    required this.desc,
+  });
+}
---
+++ lib/domain/entities/food.dart
@@ -0,0 +1,13 @@
+class Food {
+  final int? id;
+  final String name;
+  final String image;
+  final String desc;
+
+  Food({
+    this.id,
+    required this.name,
+    required this.image,
+    required this.desc,
+  });
+}
